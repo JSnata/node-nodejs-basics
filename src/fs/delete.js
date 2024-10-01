fab8024ec5fdaf5c5424e06f0c50a1400ccab0d3
JSNata <n.kulikouskaya@gmail.com>
2024-10-01 15:39:53 +0200

    feat: implements delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,28 @@
+import { access, unlink } from 'node:fs/promises';
+import { constants } from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        const fileToRemovePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+        try {
+            await access(fileToRemovePath , constants.F_OK);
+            await unlink(fileToRemovePath)
+        } catch (err) {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            } else {
+                throw err;
+            }
+        }
+    } catch (err) {
+        throw new Error(err);
+    }
 };
 
 await remove();
