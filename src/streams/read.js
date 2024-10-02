d4a9bdab6c2b51d25bbce7ec951596937295a6f5
JSNata <n.kulikouskaya@gmail.com>
2024-10-02 16:25:14 +0200

    feat: implements streams/read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const folderPath = path.join(__dirname, 'files');
+    const filePath = path.join(folderPath, 'fileToRead.txt');
+
+    const fileStream = createReadStream(filePath, { encoding: 'utf-8' });
+    fileStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    fileStream.on('end', () => {
+        console.log('\nFile reading finished.');
+    });
+
+    fileStream.on('error', (err) => {
+        console.error(err);
+    });
 };
 
 await read();
