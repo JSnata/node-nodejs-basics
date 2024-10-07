e6cb74413acf35e5abd7a5932441ab0499baeac5
JSNata <n.kulikouskaya@gmail.com>
2024-10-07 12:37:54 +0200

    feat: implements spawnChildProcess function

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,5 +1,5 @@
-// n should be received from main thread
 import { parentPort, workerData } from 'worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
